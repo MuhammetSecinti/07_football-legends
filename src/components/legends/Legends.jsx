ecc4da6036aec5aeb36883e79b4a930b435e86e6
Muhammet <secintimuhammet@gmail.com>
2025-01-18 14:32:24 +0300

    toggle

--- src/components/legends/Legends.jsx
+++ src/components/legends/Legends.jsx
@@ -1,7 +1,12 @@
 import { Card, Col, Container, Form, Row } from "react-bootstrap";
 import { data } from "../../helpers/data";
+import { useState } from "react";
+
+
 
 const Legends = () => {
+  const [show, setShow] = useState(false);
+
   return (
     <Container className="text-center">
       <Form.Control type="search" placeholder="" />
@@ -9,17 +14,27 @@ const Legends = () => {
         <Row>
           {data.map((legends) => (
             <Col className="mt-4">
-              <Card style={{ width: "18rem", height: "400px" }}>
-                <Card.Img variant="top" src={legends.img} />
-                <Card.Header>
-                  <Card.Title>{legends.name}</Card.Title>
-                </Card.Header>
-                <ul className="m-auto">
-                  {legends.statistics.map((statistic) => {
-                    return <li className="list-unstyled h5 text-start">⚽{statistic}</li>;
-                  })}
-                </ul>
-                <span>{legends.official_career}</span>
+              <Card style={{ width: '18rem' }} onClick={()=> setShow(!show)} >
+                {!show ? (
+                  <Card.Img variant="top" src={legends.img} />
+                ) : (
+                  <>
+                    <Card.Header>
+                      <Card.Title>{legends.name}</Card.Title>
+                    </Card.Header>
+                    <ul className="m-auto">
+                      {legends.statistics.map((statistic) => {
+                        return (
+                          <li className="list-unstyled h5 text-start">
+                            ⚽{statistic}
+                          </li>
+                        );
+                      })}
+                    </ul>
+
+                    <span>{legends.official_career}</span>
+                  </>
+                )}
               </Card>
             </Col>
           ))}
